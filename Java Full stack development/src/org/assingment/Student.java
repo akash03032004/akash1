43424870c6ffe41163d504383bb7bede84f2017c
jagad <jagad@DESKTOP-LVEI9QH>
2024-11-12 05:50:08 -1100

    my testing

--- Java Full stack development/src/org/assingment/Student.java
+++ Java Full stack development/src/org/assingment/Student.java
@@ -7,27 +7,27 @@ public class Student {
 		String address=a.nextLine();
 		int salesAmount=a.nextInt();
 		double phone=a.nextDouble();
-		//double commission=0.0 ;
+		double commission=0.0 ;
 		
-		//if(salesAmount>=100000) {
-			//commission=salesAmount * 0.10;
-	//	}
-		//else if (salesAmount>=50000&salesAmount<100000) {
-			//commission=salesAmount * 0.05;
-		//}
-		//else if(salesAmount>=30000&salesAmount<50000) {
-			//commission=salesAmount *0.03;
+		if(salesAmount>=100000) {
+			commission=salesAmount * 0.10;
+	}
+		else if (salesAmount>=50000&salesAmount<100000) {
+			commission=salesAmount * 0.05;
+		}
+		else if(salesAmount>=30000&salesAmount<50000) {
+			commission=salesAmount *0.03;
 		
-		//}
-		//else {
-			//commission=0.0;
-		//}
+		}
+		else {
+			commission=0.0;
+		}
 		
-		//System.out.println("details");
+		System.out.println("details");
 		System.out.println("name: "+name);
 		System.out.println("address: "+address);
 		System.out.println("phone number: "+phone);
 		System.out.println("Amount: "+salesAmount);
-		//System.out.println("commission: "+commission);
+		System.out.println("commission: "+commission);
 	}	
 }
